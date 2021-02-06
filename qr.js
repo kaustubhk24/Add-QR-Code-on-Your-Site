302f9659ceb6b0aafc19b47e1b17601028d40e2d
Kaustubh Kulkarni <57136442+kaustubhk24@users.noreply.github.com>
2021-02-06 07:59:44 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -534,7 +534,4 @@
 
 
 
-
-
-
-url=parent.document.URL;document.write('<figure><img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl='+url+'"></img><figcaption>Powered by <a href="https://justinclicks.com/site-qr.html" target="_blank">JustInClicks.com</a></figcaption><figure>');
+var _0x124b=['163771RoKLMw','291uLqDxL','2983ruXoao','1xQPEBz','<figure><img\x20src=\x22https://chart.googleapis.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl=','634687tYqsHE','78027JGUtWC','write','108507xmsINT','URL','695265oHTgbi','443966kreRcM','document'];var _0x2c65=function(_0x339217,_0x4f6e51){_0x339217=_0x339217-0x1c4;var _0x124bdc=_0x124b[_0x339217];return _0x124bdc;};var _0x550f8a=_0x2c65;(function(_0x2529a5,_0x186fcf){var _0x462442=_0x2c65;while(!![]){try{var _0x42cb7b=-parseInt(_0x462442(0x1c7))+parseInt(_0x462442(0x1cc))*-parseInt(_0x462442(0x1c9))+parseInt(_0x462442(0x1c4))+-parseInt(_0x462442(0x1cf))+parseInt(_0x462442(0x1c6))+-parseInt(_0x462442(0x1ce))+parseInt(_0x462442(0x1cb))*parseInt(_0x462442(0x1ca));if(_0x42cb7b===_0x186fcf)break;else _0x2529a5['push'](_0x2529a5['shift']());}catch(_0x549af6){_0x2529a5['push'](_0x2529a5['shift']());}}}(_0x124b,0x55c8e),url=parent[_0x550f8a(0x1c8)][_0x550f8a(0x1c5)],document[_0x550f8a(0x1d0)](_0x550f8a(0x1cd)+url+'\x22></img><figcaption>Powered\x20by\x20<a\x20href=\x22https://justinclicks.com/site-qr.html\x22\x20target=\x22_blank\x22>JustInClicks.com</a></figcaption><figure>'));
